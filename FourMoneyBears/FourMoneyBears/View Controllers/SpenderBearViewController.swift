1fd9958d756fbe9c463f44de0103ccc48f73aea9
AustinPotts <pottsevolvements@gmail.com>
2020-04-10 13:31:12 -0400

    Format for Game Questions Configured
    TODO: Fill out each questions page

---
+++ FourMoneyBears/FourMoneyBears/View Controllers/SpenderBearViewController.swift
@@ -0,0 +1,65 @@
+//
+//  SpenderBearViewController.swift
+//  FourMoneyBears
+//
+//  Created by Austin Potts on 4/10/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class SpenderBearViewController: UIViewController {
+
+    
+    
+    @IBOutlet var userHeartLife: UIButton!
+    
+    
+    @IBOutlet var answerView1: UIView!
+    @IBOutlet var answerView2: UIView!
+    @IBOutlet var answerView3: UIView!
+    @IBOutlet var answerView4: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+            updateAnswerViews()
+    }
+    
+    
+    func updateAnswerViews() {
+        
+        // View Corner Radius
+        answerView1.layer.cornerRadius = 20
+        answerView2.layer.cornerRadius = 20
+        answerView3.layer.cornerRadius = 20
+        answerView4.layer.cornerRadius = 20
+        
+        // View Borders
+        answerView1.layer.borderWidth = 5
+        answerView1.layer.borderColor = UIColor.gray.cgColor
+        answerView2.layer.borderWidth = 5
+        answerView2.layer.borderColor = UIColor.gray.cgColor
+        answerView3.layer.borderWidth = 5
+        answerView3.layer.borderColor = UIColor.gray.cgColor
+        answerView4.layer.borderWidth = 5
+        answerView4.layer.borderColor = UIColor.gray.cgColor
+        
+        
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func xTapped(_ sender: Any) {
+        
+        self.dismiss(animated: true, completion: nil)
+        
+    }
+    
+}
