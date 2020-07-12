3099f17edb3cb0511dc9a603e159574544bc24fd
AustinPotts <pottsevolvements@gmail.com>
2020-07-12 13:35:17 -0400

    Configured GiverBear Game Three + Four

--- FourMoneyBears/FourMoneyBears/View Controllers/Giver Bear/GiverBearGameFourViewController.swift
+++ FourMoneyBears/FourMoneyBears/View Controllers/Giver Bear/GiverBearGameFourViewController.swift
@@ -7,24 +7,146 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class GiverBearGameFourViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        @IBOutlet var answerView1: UIView!
+        @IBOutlet var answerView2: UIView!
+        @IBOutlet var answerView3: UIView!
+        @IBOutlet var answerView4: UIView!
+        @IBOutlet var statusBar: UIView!
+        @IBOutlet var orangeStatus: UIView!
+        
+        override func viewDidLoad() {
+            super.viewDidLoad()
+            updateAnswerViews()
+        }
+        
+        
+        func updateAnswerViews() {
+            
+            // View Corner Radius
+            answerView1.layer.cornerRadius = 20
+            answerView2.layer.cornerRadius = 20
+            answerView3.layer.cornerRadius = 20
+            answerView4.layer.cornerRadius = 20
+            
+            // View Borders
+            answerView1.layer.borderWidth = 5
+            answerView1.layer.borderColor = UIColor.gray.cgColor
+            answerView2.layer.borderWidth = 5
+            answerView2.layer.borderColor = UIColor.gray.cgColor
+            answerView3.layer.borderWidth = 5
+            answerView3.layer.borderColor = UIColor.gray.cgColor
+            answerView4.layer.borderWidth = 5
+            answerView4.layer.borderColor = UIColor.gray.cgColor
+            
+            statusBar.layer.cornerRadius = 9
+            orangeStatus.layer.cornerRadius = 9
+            
+            
+        }
+        
+        
+        // MARK: - Navigation
+        
+        // In a storyboard-based application, you will often want to do a little preparation before navigation
+        //    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        //        if segue.identifier == "CorrectAnswerOneSegue" {
+        //            if let detailVC = segue.destination as? SpenderBearGameOneViewController {
+        //
+        //            }
+        //        }
+        //    }
+        
+        @IBAction func xTapped(_ sender: Any) {
+            
+            self.performSegue(withIdentifier: "unwindSegue", sender: nil)
+            
+        }
+        
+        func animateStatusBar(){
+            UIView.animate(withDuration: 2, animations: {
+                // self.orangeStatus.frame.origin.x -= 70
+                self.orangeStatus.translatesAutoresizingMaskIntoConstraints = false
+                self.orangeStatus.layer.frame.size.width += 15
+                
+                
+            })
+        }
+        
+        
+        @IBAction func answerView1Tapped(_ sender: Any) {
+            
+           SCLAlertView().showError("Wrong Answer", subTitle: "Try Again!")
+        
+        }
+        
+        
+        //MARK: - ALERT
+        func showCorrectAlert(){
+            
+            
+            
+            let alert = UIAlertController(title: "That's Correct!", message: "You should only spend 20 percent of your savings.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Yay", style: .default, handler: { (action) in
+                self.performSegue(withIdentifier: "CorrectAnswerTwoSegue", sender: self)
+            }))
+            
+            present(alert, animated: true)
+            
+            let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+            subview.layer.cornerRadius = 1
+            subview.backgroundColor = UIColor.green
+            
+            
+            
+        }
+        
+        
+        func showWrongAlert(){
+            
+            
+            
+            let alert = UIAlertController(title: "That's Inorrect!", message: "You should only spend 20 percent of your savings.", preferredStyle: .alert)
+            
+            alert.addAction(UIAlertAction(title: "Next", style: .destructive, handler: nil))
+            present(alert, animated: true)
+            let subview = (alert.view.subviews.first?.subviews.first?.subviews.first!)! as UIView
+            subview.layer.cornerRadius = 1
+            subview.backgroundColor = UIColor.red
+            
+        }
+        
+        @IBAction func answerView2Tapped(_ sender: Any) {
+            let alertViewResponder: SCLAlertViewResponder = SCLAlertView().showCustom("Good job, You won!", subTitle: "Reward: 10xp", color: UIColor.white, icon: UIImage(systemName: "flame.fill")!)
+            
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 4){
+                alertViewResponder.close()
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+                self.performSegue(withIdentifier: "unwindSegue", sender: self)
+                
+            }
+            animateStatusBar()
+                   
+            
+        }
+        
+        
+        @IBAction func answerView3Tapped(_ sender: Any) {
+            SCLAlertView().showError("Wrong Answer", subTitle: "Try Again!")
+        }
+        
+        
+        @IBAction func answerView4Tapped(_ sender: Any) {
+            SCLAlertView().showError("Wrong Answer", subTitle: "Try Again!")
+        }
+        
+        
     }
-    */
-
-}
--- FourMoneyBears/FourMoneyBears/View Controllers/Giver Bear/GiverBearGameThreeViewController.swift
+++ FourMoneyBears/FourMoneyBears/View Controllers/Giver Bear/GiverBearGameThreeViewController.swift
@@ -7,24 +7,103 @@
 //
 
 import UIKit
+import SCLAlertView
 
 class GiverBearGameThreeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+      //Outlets
+         @IBOutlet var orangeStatus: UIView!
+         @IBOutlet var answerView: UIView!
+         @IBOutlet var choiceViewOne: UIView!
+         @IBOutlet var choiceViewTwo: UIView!
+         @IBOutlet var choiceViewThree: UIView!
+         @IBOutlet var choiceViewFour: UIView!
+         @IBOutlet var choiceViewFive: UIView!
+         @IBOutlet var checkAnswerButton: UIButton!
+         
+         @IBOutlet var whiteStatus: UIView!
+         
+         //MARK: - Life Cycle
+         override func viewDidLoad() {
+             super.viewDidLoad()
+             
+             setUpMiscViews()
+             setUpAnswerViews()
 
-        // Do any additional setup after loading the view.
-    }
-    
+         }
+         
+         func setUpMiscViews(){
+             orangeStatus.layer.cornerRadius = 9
+             whiteStatus.layer.cornerRadius = 9
+             checkAnswerButton.layer.cornerRadius = 20
+             
+         }
+         
+         func setUpAnswerViews(){
+             answerView.layer.cornerRadius = 5
+             choiceViewOne.layer.cornerRadius = 5
+             choiceViewTwo.layer.cornerRadius = 5
+             choiceViewThree.layer.cornerRadius = 5
+             choiceViewFour.layer.cornerRadius = 5
+             choiceViewFive.layer.cornerRadius = 5
+         }
+         
+         func animateStatusBar(){
+               UIView.animate(withDuration: 2, animations: {
+                    // self.orangeStatus.frame.origin.x -= 70
+                   self.orangeStatus.translatesAutoresizingMaskIntoConstraints = false
+                   self.orangeStatus.layer.frame.size.width += 73
+                   
+                  
+                 })
+           }
+         
+         
 
-    /*
-    // MARK: - Navigation
+      //Actions
+         
+         @IBAction func xTapped(_ sender: Any) {
+             self.performSegue(withIdentifier: "unwindSegue", sender: nil)
+         }
+         
+         
+         @IBAction func checkAnswerTapped(_ sender: Any) {
+             if choiceViewFive.frame.intersects(answerView.frame){
+                SCLAlertView().showSuccess("Good Job!", subTitle: "Next")
+                DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                    self.performSegue(withIdentifier: "correctAnswerThreeSegue", sender: self)
+                }
+                 animateStatusBar()
+                 } else {
+                     SCLAlertView().showError("Wrong Answer", subTitle: "Try Again!")
+                 }
+         }
+         
+             @IBAction func panView(_ sender: UIPanGestureRecognizer) {
+                 let translation = sender.translation(in: self.view)
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
-}
+                 if let viewToDrag = sender.view {
+                     viewToDrag.center = CGPoint(x: viewToDrag.center.x + translation.x,
+                         y: viewToDrag.center.y + translation.y)
+                     sender.setTranslation(CGPoint(x: 0, y: 0), in: viewToDrag)
+                     
+         //            switch sender.state {
+         //            case .began, .changed:
+         //                choiceViewOne.center = CGPoint(x: choiceViewOne.center.x + translation.x, y: choiceViewOne.center.y + translation.y)
+         //                sender.setTranslation(CGPoint.zero, in: view)
+         //                break
+         //            case .ended:
+         //                if choiceViewOne.frame.intersects(answerView.frame) {
+         //
+         //                    UIView.animate(withDuration: 0.3) {
+         //
+         //                    }
+         //                }
+         //
+         //            default:
+         //                break
+         //            }
+                 }
+             }
+         
+     }
