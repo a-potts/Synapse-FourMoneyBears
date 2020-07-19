cd358170847cf45687c351a2d4437645c726598c
AustinPotts <pottsevolvements@gmail.com>
2020-07-18 21:43:38 -0400

    Added Image Helper Extension + Custom Tab Bar Color

---
+++ FourMoneyBears/FourMoneyBears/Helpers/Extensions.swift
@@ -0,0 +1,56 @@
+//
+//  Extensions.swift
+//  FourMoneyBears
+//
+//  Created by Austin Potts on 7/18/20.
+//  Copyright © 2020 Lambda School. All rights reserved.
+//
+
+import Foundation
+import Foundation
+import UIKit
+
+let imageCache = NSCache<AnyObject, AnyObject>()
+
+extension UIImageView {
+    
+    func loadImageUsingCacheWithUrlString(urlString: String) {
+        
+        //self.image = nil
+    
+      //check cache for image first
+        if let cachedImage = imageCache.object(forKey: urlString as AnyObject) as? UIImage {
+            self.image = cachedImage
+            return
+        }
+        
+        //otherwise fire off a new download
+        let url = URL(string: urlString)
+        
+        URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            
+            if let error = error {
+                print("Error getting image: \(error)")
+                return
+            }
+            
+            DispatchQueue.main.async {
+                
+                if let downloadedImage = UIImage(data: data!) {
+                    imageCache.setObject(downloadedImage, forKey: urlString as AnyObject)
+                    
+                    
+                    self.image = downloadedImage
+                }
+                
+             
+            }
+            
+            
+        }.resume()
+        
+    }
+            
+    
+}
+
